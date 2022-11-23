c2e8b3c9c2d611608db75a970748697be4403831
JC <jc.alonsome@gmail.com>
2022-11-23 11:19:34 +0100

    menu animado a medias

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,10 +1,13 @@
 const menu = document.getElementById('menu')
 const menuIcon = document.getElementById('menu-icon')
 const rootStyles= document.documentElement.style;
+const menuItem = document.getElementById('menu-item')
 
 menuIcon.addEventListener('click' , () => {
     rootStyles.setProperty('--transition-duration',1 + 's')
     menu.classList.toggle('menu--show')
+    menuItem.classList.toggle('menu__item--show')
+
 });
 
 window.addEventListener('resize' ,() => {
