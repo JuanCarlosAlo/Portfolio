a7ab15b3389a2d9b942c36e0988c9196c06c9715
JuanCarlosAlo <jc.alonsome@gmail.com>
2022-11-23 14:27:31 +0100

    menu sin animetion

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,12 +1,11 @@
 const menu = document.getElementById('menu')
 const menuIcon = document.getElementById('menu-icon')
 const rootStyles= document.documentElement.style;
-const menuItem = document.getElementById('menu-item')
+
 
 menuIcon.addEventListener('click' , () => {
     rootStyles.setProperty('--transition-duration',1 + 's')
     menu.classList.toggle('menu--show')
-    menuItem.classList.toggle('menu__item--show')
 
 });
 
