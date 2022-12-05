d20d6c6ed28afcc7db38b77ed863e47f9c0a26d4
JC <jc.alonsome@gmail.com>
2022-12-05 17:05:00 +0100

    modal

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,134 +1,126 @@
-const menu = document.getElementById('menu')
-const menuIcon = document.getElementById('menu-icon')
+const menu = document.getElementById("menu");
+const menuIcon = document.getElementById("menu-icon");
 const rootStyles = document.documentElement.style;
-const arrowLeft = document.getElementById('arrow-left-slider')
-const arrowRight = document.getElementById('arrow-right-slider')
-const slider = document.getElementById('slider')
-const menuItem = document.getElementById('menu-item')
+const arrowLeft = document.getElementById("arrow-left-slider");
+const arrowRight = document.getElementById("arrow-right-slider");
+const slider = document.getElementById("slider");
+const menuItem = document.getElementById("menu-item");
 
-const modalCross = document.getElementById('cross-icon')
+const modalCross = document.getElementById("cross-icon");
 
+const allMenuLinks = document.querySelectorAll(".menu__link");
+const allMenuItem = document.querySelectorAll(".menu__item");
 
-const allMenuLinks = document.querySelectorAll('.menu__link')
-const allMenuItem = document.querySelectorAll('.menu__item')
-
-const design = document.getElementById('design')
-const modal = document.getElementById('modal')
-const modalImg = document.getElementById('modal-img')
-const modalTitle = document.getElementById('modal-title')
-const modalText = document.getElementById('modal-text')
+const design = document.getElementById("design");
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-img");
+const modalTitle = document.getElementById("modal-title");
+const modalText = document.getElementById("modal-text");
 
 const data = [
-    {
-        title: 'Publicidad',
-        src: 'assets/images/modal-publicitario2.jpg',
-        text: 'Modal diseño publicitario',
-    },
-    {
-        title: 'Proyecto Your Reality',
-        src: 'assets/images/modal-your-reality.jpg',
-        text: 'Modal proyecto Your Reality',
-    },
-    {
-        title: 'Proyectos OMD',
-        src: 'assets/images/modal-omd',
-        text: 'Modal proyectos OMD',
-    },
-    {
-        title: 'Ilustracion',
-        src: 'assets/images/modal-ilustracion',
-        text: 'Modal Ilustracion'
-    },
-    {
-        title: 'Tokyo Stand',
-        src: 'assets/images/modal-tokyo',
-        text: 'Modal Tokyo Stand',
-    },
-    
+  {
+    title: "Publicidad",
+    src: "assets/images/modal-publicitario2.jpg",
+    text: "Modal diseño publicitario",
+  },
+  {
+    title: "Proyecto Your Reality",
+    src: "assets/images/modal-your-reality.jpg",
+    text: "Modal proyecto Your Reality",
+  },
+  {
+    title: "Proyectos OMD",
+    src: "assets/images/modal-omd.jpg",
+    text: "Modal proyectos OMD",
+  },
+  {
+    title: "Ilustracion",
+    src: "assets/images/modal-ilustration.jpg",
+    text: "Modal Ilustracion",
+  },
+  {
+    title: "Tokyo Stand",
+    src: "assets/images/modal-tokyo.jpg",
+    text: "Modal Tokyo Stand",
+  },
 ];
 
-design.addEventListener('click', (e) => {
+design.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("grid-container")) {
     let currentElement;
-    if(e.target.classList.contains('design')){
-        currentElement = e.target
-    }else if(e.target.parentElement.classList.contains('design')){
-        currentElement = e.target.parentElement
-    }else if(e.target.classList.contains('design__header__title')){
-        currentElement = e.target.parentElement.parentElement
+
+    if (e.target.classList.contains("design")) {
+      currentElement = e.target;
+    } else if (e.target.parentElement.classList.contains("design")) {
+      currentElement = e.target.parentElement;
+    } else if (e.target.classList.contains("design__header__title")) {
+      currentElement = e.target.parentElement.parentElement;
     }
     const index = Array.from(design.children).indexOf(currentElement);
-    
-    modal.classList.add('modal--show')
-    console.log(design.children)
-    console.log(e.target)
+
+    modal.classList.add("modal--show");
+    console.log(design.children);
+    console.log(e.target);
 
     modalTitle.textContent = data[index].title;
     modalImg.src = data[index].src;
     modalText.textContent = data[index].text;
+  }
 });
 
-
-const allSliderContent = Array.from(document.querySelectorAll('.slider__content'))
+const allSliderContent = Array.from(
+  document.querySelectorAll(".slider__content")
+);
 
 let counter = 0;
 let translate = 0;
 
-const moveSlide = () =>{
-    rootStyles.setProperty('--slider-translate', 100 * translate * -1 + '%')
-}
+const moveSlide = () => {
+  rootStyles.setProperty("--slider-translate", 100 * translate * -1 + "%");
+};
 
 const nextSlide = () => {
-    
-    if (counter >= allSliderContent.length -1) {
-        counter = 0;
-        translate=0
-    }
-    else{
-        counter = counter + 1;
-        translate = translate + 1
-    }
+  if (counter >= allSliderContent.length - 1) {
+    counter = 0;
+    translate = 0;
+  } else {
+    counter = counter + 1;
+    translate = translate + 1;
+  }
 
-    moveSlide()
-}
+  moveSlide();
+};
 
 const prevSlide = () => {
-    if (counter <= 0){
-        counter = allSliderContent.length -1
-        translate = allSliderContent.length -1
-    }
-    else{
-        counter = counter - 1;
-        translate = translate -1;
-    }
+  if (counter <= 0) {
+    counter = allSliderContent.length - 1;
+    translate = allSliderContent.length - 1;
+  } else {
+    counter = counter - 1;
+    translate = translate - 1;
+  }
 
-    moveSlide()
-}
+  moveSlide();
+};
 
-arrowRight.addEventListener('click', nextSlide)
-arrowLeft.addEventListener('click', prevSlide)
+arrowRight.addEventListener("click", nextSlide);
+arrowLeft.addEventListener("click", prevSlide);
 
-
-
-menuIcon.addEventListener('click' , () => {
-    rootStyles.setProperty('--transition-duration',1 + 's')
-    menu.classList.toggle('menu--show')
-    allMenuLinks.forEach(menuLink=>{
-        menuLink.classList.toggle('menu__link--show')
-    })
-    allMenuItem.forEach(menuItem=>{
-        menuItem.classList.toggle('menu__item--show')
-    })
-    
-
+menuIcon.addEventListener("click", () => {
+  rootStyles.setProperty("--transition-duration", 1 + "s");
+  menu.classList.toggle("menu--show");
+  allMenuLinks.forEach((menuLink) => {
+    menuLink.classList.toggle("menu__link--show");
+  });
+  allMenuItem.forEach((menuItem) => {
+    menuItem.classList.toggle("menu__item--show");
+  });
 });
 
+modalCross.addEventListener("click", () => {
+  modal.classList.toggle("modal--show");
+});
 
-modalCross.addEventListener('click' , () =>{
-    modal.classList.toggle('modal--show')
-})
-
-window.addEventListener('resize' ,() => {
-    rootStyles.setProperty('--transition-duration',0)
-})
-
-
+window.addEventListener("resize", () => {
+  rootStyles.setProperty("--transition-duration", 0);
+});
