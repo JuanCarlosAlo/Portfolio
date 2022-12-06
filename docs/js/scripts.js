826a5460ac452e99b56a1f2aef197aa89b2a6909
JC <jc.alonsome@gmail.com>
2022-12-06 19:55:36 +0100

    text

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -13,6 +13,7 @@ const allMenuItem = document.querySelectorAll(".menu__item");
 
 const design = document.getElementById("design");
 const modal = document.getElementById("modal");
+const modalContainer = document.getElementById("modal-container");
 const modalImg = document.getElementById("modal-img");
 const modalTitle = document.getElementById("modal-title");
 const modalText = document.getElementById("modal-text");
@@ -21,27 +22,27 @@ const data = [
   {
     title: "Publicidad",
     src: "assets/images/modal-publicitario2.jpg",
-    text: "Modal diseño publicitario,Lorem ipsum dolor sit amet consectetur adipiscing elit viverra pharetra et, odio curabitur lobortis netus aliquam vestibulum fringilla dui duis euismod, penatibus ultrices id aliquet tortor",
+    text: "Como diseñador he realizado varios proyectos en el entorno publicitario como carteleria aplicada a eventos o desarrollo de marca. Aqui muestro varios ejemplos de carteles desarrollados por mi con propuesta de aplicacion en un entorno real.",
   },
   {
     title: "Proyecto Your Reality",
     src: "assets/images/modal-your-reality.jpg",
-    text: "Modal proyecto Your Reality,Lorem ipsum dolor sit amet consectetur adipiscing elit viverra pharetra et, odio curabitur lobortis netus aliquam vestibulum fringilla dui duis euismod, penatibus ultrices id aliquet tortor",
+    text: "Como proyecto de fin de carrera, realizé un proyecto de un laboratorio cultural de desarrollo de videojuegos, en el cual desarrolle la marca, el branding y la propuesta de marketing, la propuesta espacial para el desarrollo del evento y una propuesta de diseño de objeto para el evento. Aqui se muestran varias piezas del proyecto realizado.",
   },
   {
     title: "Proyectos OMD",
     src: "assets/images/modal-omd.jpg",
-    text: "Modal proyectos OMD,Lorem ipsum dolor sit amet consectetur adipiscing elit viverra pharetra et, odio curabitur lobortis netus aliquam vestibulum fringilla dui duis euismod, penatibus ultrices id aliquet tortor",
+    text: "En mi tiempo trabajando como diseñador en la empresa OMD realice varias piezas visuales, logotipos y propuestas de branding para marcas internas de la empresa. Aqui muestro algunas piezas realizadas por mi y miembros de la empresa OMD.",
   },
   {
     title: "Ilustracion",
     src: "assets/images/modal-ilustration.jpg",
-    text: "Modal Ilustracion,Lorem ipsum dolor sit amet consectetur adipiscing elit viverra pharetra et, odio curabitur lobortis netus aliquam vestibulum fringilla dui duis euismod, penatibus ultrices id aliquet tortor",
+    text: "Como hobby personal, aprendo a realizar ilustraciones digitales. La mayoria de las ilustraciones quedan innacabadas o en simples bocetos. Algunas ilustraciones estan finalizadas, pero aun me queda un largo camino de aprendizaje y desarrollo personal.",
   },
   {
     title: "Tokyo Stand",
     src: "assets/images/modal-tokyo.jpg",
-    text: "Modal Tokyo Stand,Lorem ipsum dolor sit amet consectetur adipiscing elit viverra pharetra et, odio curabitur lobortis netus aliquam vestibulum fringilla dui duis euismod, penatibus ultrices id aliquet tortor",
+    text: "Este proyecto fue un proyecto realizado en la universidad, en la que desarrolle una estructura efímera que sirviera para la representacion de un país, en mi caso fue Japon. Realizé los planos estructurales, de iluminacion y de mobiliario y despues mostré el resultado final con un renderizado 3D.",
   },
 ];
 
@@ -59,9 +60,7 @@ design.addEventListener("click", (e) => {
     const index = Array.from(design.children).indexOf(currentElement);
 
     modal.classList.add("modal--show");
-    console.log(design.children);
-    console.log(e.target);
-
+    modalContainer.classList.add("modal__container--show");
     modalTitle.textContent = data[index].title;
     modalImg.src = data[index].src;
     modalText.textContent = data[index].text;
@@ -118,7 +117,8 @@ menuIcon.addEventListener("click", () => {
 });
 
 modalCross.addEventListener("click", () => {
-  modal.classList.toggle("modal--show");
+  modal.classList.remove("modal--show");
+  modalContainer.classList.remove("modal__container--show");
 });
 
 window.addEventListener("resize", () => {
